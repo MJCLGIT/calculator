84489baaaef2f55f0c7d2659a7410065fdf126ce
MJCL <joule_patrols01@icloud.com>
2023-05-11 00:50:41 +0100

    [feat] create functions for add, subtract, multiply, divide.

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstInputtedNumber = 
+let chosenOperator = 
+let secondInputtedNumber = 
+
+function add(num1, num2) {
+    let result = num1 + num2;
+}
+
+function subtract(num1, num2) {
+    let result = num1 - num2;
+}
+
+function multiply (num1, num2) {
+    let result = num1 * num2;
+}
+
+function divide (num1, num2) {
+    let result = num1 / num2;
+}
