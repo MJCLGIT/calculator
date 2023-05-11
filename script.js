654cbd58bd79c01592395bbadaf02aeba7378216
MJCL <joule_patrols01@icloud.com>
2023-05-11 13:34:11 +0100

    [feat] create facade and buttons for calculator interface.

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
-let firstInputtedNumber = 
-let chosenOperator = 
-let secondInputtedNumber = 
-
 function add(num1, num2) {
     let result = num1 + num2;
+    return result;
 }
 
 function subtract(num1, num2) {
     let result = num1 - num2;
+    return result;
 }
 
 function multiply (num1, num2) {
     let result = num1 * num2;
+    return result;
 }
 
 function divide (num1, num2) {
     let result = num1 / num2;
+    return result;
+}
+
+function operate (operator, num1, num2) {
+    if (opertor === "+"){
+        return add(num1, num2);
+    }
+    else if (operator === "-"){
+        return subtract(num1, num2);
+    }
+    else if (operator === "*"){
+        return multiply(num1, num2);
+    }
+    else if (operator === "/"){
+        return divide(num1, num2);
+    }
 }
