408dd4aa76b29e693b8dd5dc898cc163538a7ca4
MJCL <joule_patrols01@icloud.com>
2023-05-16 13:55:15 +0100

    [feat] add event listener to operator buttons. Clear display if clear button is clicked

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ function operate (operator, num1, num2) {
         return divide(num1, num2);
     }
 }
+
+let calculatorDisplay = document.getElementById('displayHeading');
+let numberSelection = document.getElementsByClassName('numberButtons');
+let operatorButton = document.getElementsByClassName("operatorButtons"); 
+
+// clear display
+
+
+// add event listener to each operator button
+for (let i = 0; i < operatorButton.length; i++) {
+    operatorButton[i].addEventListener('click', function(){
+        const symbol = operatorButton[i].textContent;
+        console.log('Operator Clicked:', symbol);
+            // clear display if 'clear' button is clicked
+            if (symbol === 'Clear') {
+                calculatorDisplay.innerHTML = '';
+            }
+    });
+}
