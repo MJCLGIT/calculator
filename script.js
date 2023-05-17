1d300667d7a2b25326cbdf64d5e3dcf2c4584812
MJCL <joule_patrols01@icloud.com>
2023-05-17 12:53:38 +0100

    [feat] create global variables which contain result of operator and number button click result. [change] display now shows 0 instead of nothing when clear button is clicked.

--- script.js
+++ script.js
@@ -36,23 +36,31 @@ function operate (operator, num1, num2) {
 let calculatorDisplay = document.getElementById('displayHeading');
 let numberButton = document.getElementsByClassName('numberButtons');
 let operatorButton = document.getElementsByClassName("operatorButtons"); 
+// operator accessible globally, pulled from local event listener
+let globalOperator;
+// number accessible globally, pulled from local event listener
+let globalNumber;
 
 // add event listener to each operator button
 for (let i = 0; i < operatorButton.length; i++) {
     operatorButton[i].addEventListener('click', function(){
         const symbol = operatorButton[i].textContent;
-        console.log('Operator Clicked:', symbol);
+        // console.log('Operator Clicked:', symbol);
             // clear display if 'clear' button is clicked
             if (symbol === 'Clear') {
-                calculatorDisplay.innerHTML = '';
+                calculatorDisplay.innerHTML = '0';
+            }
+            else {
+                globalOperator = symbol;
             }
     });
 }
 
-// add event listener to each operator button
+// add event listener to each number button
 for (let i = 0; i < numberButton.length; i ++) {
     numberButton[i].addEventListener('click', function(){
         const number = numberButton[i].textContent;
-        console.log('Number Clicked:', number);
+        // console.log('Number Clicked:', number);
+        globalNumber = number;
     });
 }
