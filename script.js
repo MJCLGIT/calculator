3b82bc07c3565d825d852a5e46e7086977592dfe
MJCL <joule_patrols01@icloud.com>
2023-06-07 20:43:43 +0100

    "cleared"

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let calculatorDisplay = document.getElementById('displayHeading');
+let numberButton = document.getElementsByClassName('numberButtons');
+let operatorButton = document.getElementsByClassName("operatorButtons"); 
+let equalsButton = document.getElementById("equalsButton");
+// operator accessible globally, pulled from local event listener
+let globalOperator = null;
+// number accessible globally, pulled from local event listener
+let globalNumber = null;
+
 function add(num1, num2) {
     let result = num1 + num2;
     return result;
@@ -18,42 +27,46 @@ function divide (num1, num2) {
     return result;
 }
 
-function operate (operator, num1, num2) {
-    if (operator === "+"){
+function operate (globalOperator, num1, num2) {
+    if (globalOperator === "+"){
         return add(num1, num2);
     }
-    else if (operator === "-"){
+    else if (globalOperator === "-"){
         return subtract(num1, num2);
     }
-    else if (operator === "*"){
+    else if (globalOperator === "*"){
         return multiply(num1, num2);
     }
-    else if (operator === "/"){
+    else if (globalOperator === "/"){
         return divide(num1, num2);
     }
 }
-
-let calculatorDisplay = document.getElementById('displayHeading');
-let numberButton = document.getElementsByClassName('numberButtons');
-let operatorButton = document.getElementsByClassName("operatorButtons"); 
-// operator accessible globally, pulled from local event listener
-let globalOperator;
-// number accessible globally, pulled from local event listener
-let globalNumber;
 
 // add event listener to each operator button
 for (let i = 0; i < operatorButton.length; i++) {
     operatorButton[i].addEventListener('click', function(){
         const symbol = operatorButton[i].textContent;
-        // console.log('Operator Clicked:', symbol);
             // clear display if 'clear' button is clicked
             if (symbol === 'Clear') {
                 calculatorDisplay.innerHTML = '0';
+                globalOperator = null;
+                globalNumber = null;
             }
             else {
-                calculatorDisplay.innerHTML = operatorButton[i].textContent;
+                // Check if there is an existing operator and number
+                if (globalOperator) {
+                    const num1 = parseFloat(globalNumber);
+                    const num2 = parseFloat(calculatorDisplay.innerHTML);
+                    const result = operate(globalOperator, num1, num2);
+                    calculatorDisplay.innerHTML = result;
+                    globalNumber = result;
+                    globalOperator = symbol;
+            }
+            else {
+                calculatorDisplay.innerHTML = symbol;
                 globalOperator = symbol;
             }
+        }
     });
 }
 
@@ -61,8 +74,33 @@ for (let i = 0; i < operatorButton.length; i++) {
 for (let i = 0; i < numberButton.length; i ++) {
     numberButton[i].addEventListener('click', function(){
         const number = numberButton[i].textContent;
-        calculatorDisplay.innerHTML = numberButton[i].textContent;
-        // console.log('Number Clicked:', number);
-        globalNumber = number;
+        if (calculatorDisplay.innerHTML === '0') {
+            calculatorDisplay.innerHTML = number;
+        } else {
+            calculatorDisplay.innerHTML += number;
+        }
+        globalNumber = calculatorDisplay.innerHTML;
     });
 }
+
+// add event listener to the equals button
+equalsButton.addEventListener('click', function () {
+    if (globalOperator && globalNumber) {
+        console.log('globalNumber:', globalNumber);
+        console.log('calculatorDisplay.innerHTML:', calculatorDisplay.innerHTML);
+        const num1 = parseFloat(globalNumber);
+        const num2 = parseFloat(calculatorDisplay.innerHTML.trim());
+        console.log('globalOperator:', globalOperator);
+        console.log('num1:', num1);
+        console.log('num2:', num2);
+        if (isNaN(num1) || isNaN(num2)) {
+            console.log('Invalid numbers');
+            return;
+        }
+        const result = operate(globalOperator, num1, num2);
+        console.log('result:', result);
+        calculatorDisplay.innerHTML = result.toString();
+        globalNumber = result.toString();
+        globalOperator = null;
+    }
+});
